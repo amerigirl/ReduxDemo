f12d10e69bf55d794d379da0c6011c48afedaf9e
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-11-03 11:59:02 -0800

    updated index to remove depricated createStore in favor of configureStore

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-console.log('from index.js')
 
+const redux = require("redux");
+const {configureStore} = require("@reduxjs/toolkit");
+
+//const action type
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+//action creatior
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+//(previousState, action) => newState
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = configureStore({reducer});
+console.log("initialState", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+ 
+unsubscribe();
 
 //this keeps the terminal open until you hit enter--it closes too fast!
 const readline = require("readline");
