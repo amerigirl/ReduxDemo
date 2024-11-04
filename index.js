5537e280fa3f178228c5d7f23d6d85571ea6354a
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-11-03 22:21:03 -0800

    practicing nested states

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-
 const redux = require("redux");
-const {configureStore} = require("@reduxjs/toolkit");
+const { configureStore } = require("@reduxjs/toolkit");
+const bindActionCreators = redux.bindActionCreators;
+const combineReducers = redux.combineReducers;
 
 //const action type
 const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICE_CREAM_ORDERED = "ICE_CREAM_ORDERED";
+const ICE_CREAM_RESTOCKED = "ICE_CREAM_RESTOCKED";
 
 //action creatior
+//payload is more generic for restocking more than one item, while quantity is more specific to the idea of ordering a specific amount; this is a matter of convention
+
 function orderCake() {
   return {
     type: CAKE_ORDERED,
@@ -13,36 +19,106 @@ function orderCake() {
   };
 }
 
-const initialState = {
+function orderIceCream() {
+  return { type: ICE_CREAM_ORDERED, quantity: 1 };
+}
+
+function restockIceCream(payload = 1) {
+  return {
+    type: ICE_CREAM_RESTOCKED,
+    payload: payload,
+  };
+}
+function restockCake(payload = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: payload,
+  };
+}
+
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCreams: 20
+// };
+
+const initialCakeState = {
   numOfCakes: 10,
 };
 
-//(previousState, action) => newState
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+    default: 
+      return state;
+  }
+};
+
+//(previousState, action) => newState
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case ICE_CREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    case ICE_CREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
+      };
     default:
       return state;
   }
 };
 
-const store = configureStore({reducer});
+//combining reducers
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer
+})
+
+
+const store = configureStore({reducer: rootReducer});
 console.log("initialState", store.getState());
 const unsubscribe = store.subscribe(() =>
   console.log("updated state", store.getState())
 );
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
- 
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch({type: CAKE_RESTOCKED, payload:3});
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderIceCream, restockIceCream },
+  store.dispatch
+);
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+actions.restockCake(3);
+actions.orderIceCream();
+actions.orderIceCream();
+actions.orderIceCream();
+actions.restockIceCream(3);
+
 unsubscribe();
 
 //this keeps the terminal open until you hit enter--it closes too fast!
 const readline = require("readline");
+const { type } = require("os");
+const { clear } = require("console");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
---
+++ nestedState.js
@@ -0,0 +1,62 @@
+const redux = require("redux");
+const { configureStore } = require("@reduxjs/toolkit");
+
+const initialState = {
+  name: "Be-Asia",
+  address: {
+    street: "14121 44th ave S",
+    city: "Tukwila",
+    state: "WA",
+  },
+};
+
+//action type
+const STREET_UPDATED = "STREET_UPDATED";
+
+//action creator
+const updateStreet = (street) => {
+  return {
+    type: STREET_UPDATED,
+    payload: street,
+  };
+};
+
+const updateStreetReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STREET_UPDATED:
+      return {
+        ...state,
+        address: {
+          ...state.address,
+          street: action.payload,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+const store = configureStore({
+  reducer: {
+    user: updateStreetReducer,
+  },
+});
+
+console.log("initialState", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+
+store.dispatch(updateStreet("4337 N Denver Ave"));
+unsubscribe();
+
+//this keeps the terminal open until you hit enter--it closes too fast!
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Press Enter to exit...", () => {
+  rl.close();
+});
